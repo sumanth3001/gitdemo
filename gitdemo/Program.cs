421266b7346fb0e82ae87423fc01539493e80f2b
Sumanth Kulkarni <sumanth.k@keka.com>
2023-11-08 11:44:27 +0530

    m c

--- gitdemo/Program.cs
+++ gitdemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gitdemo;
+using System;
 
 namespace GitDemo
 {
@@ -7,6 +8,10 @@ namespace GitDemo
         public static void Main(string[] args)
         {
             Console.WriteLine("Its a git demo");
+            Console.WriteLine("Iam gonna use another class");
+
+            ChildClass childClass = new ChildClass();
+            childClass.DoSomeShit();
         }
     }
 }
